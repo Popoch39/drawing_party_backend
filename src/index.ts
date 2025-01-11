08ae118042533db079a9dd21f1d1be1c4fbbec16
Louis Pocheron <louis.pocheron@davikingcode.com>
2025-01-12 00:39:24 +0100

    feat: add session

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,19 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "defaultsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // Utiliser des cookies sécurisés en production
+    },
+  }),
+);
+
 initPassport(app);
+
 app.use(authRoutes);
 app.use("/api", userRoutes);
 
